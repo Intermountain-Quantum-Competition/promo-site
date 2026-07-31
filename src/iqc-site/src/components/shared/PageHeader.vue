ee58ce375302d1a02ac4ea3f1f1b3278e9d02d28
Caleb Dougal <dougalcaleb@gmail.com>
2026-07-30 22:55:28 -0600

    Add initial landing page hero section and header

---
+++ src/iqc-site/src/components/shared/PageHeader.vue
@@ -0,0 +1,88 @@
+<template>
+	<header class="w-full flex flex-col items-center">
+		<img :src="iqcInlineLogo" class="w-80 mt-10" />
+		<div class="flex items-center mt-6">
+			<img :src="byuLogo" class="h-10" />
+			●
+			<p class="font-header text-3xl ml-4">Intermountain Quantum Competition</p>
+		</div>
+		<div class="flex items-center mt-6">
+			<RouterLink to="/">
+				<div
+					:class="[
+						'font-mono font-bold text-sm tracking-wider cursor-pointer',
+						{ 'text-gold': routeActive(['/', '/home']) },
+					]"
+				>
+					EVENT
+				</div>
+			</RouterLink>
+			<div class="vspacer"></div>
+			<RouterLink to="/sponsors">
+				<div
+					:class="[
+						'font-mono font-bold text-sm tracking-wider cursor-pointer',
+						{ 'text-gold': routeActive(['/sponsors']) },
+					]"
+				>
+					SPONSORS
+				</div>
+			</RouterLink>
+			<div class="vspacer"></div>
+			<RouterLink to="/students">
+				<div
+					:class="[
+						'font-mono font-bold text-sm tracking-wider cursor-pointer',
+						{ 'text-gold': routeActive(['/students']) },
+					]"
+				>
+					STUDENTS
+				</div>
+			</RouterLink>
+			<div class="vspacer"></div>
+			<RouterLink to="/about">
+				<div
+					:class="[
+						'font-mono font-bold text-sm tracking-wider cursor-pointer',
+						{ 'text-gold': routeActive(['/about']) },
+					]"
+				>
+					ABOUT
+				</div>
+			</RouterLink>
+		</div>
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import iqcInlineLogo from '@/assets/branding/IQC/IQC Inline.svg';
+import byuLogo from '@/assets/branding/BYU/BYU monogram_white.svg';
+
+export default defineComponent({
+	components: {},
+	mixins: [],
+	props: {},
+	data() {
+		return {
+			iqcInlineLogo,
+			byuLogo,
+		};
+	},
+	computed: {},
+	methods: {
+		routeActive(routes: Array<string>) {
+			return routes.includes(this.$route.path);
+		},
+	},
+});
+</script>
+
+<style scoped>
+@reference "@/theme.css";
+
+.vspacer {
+	width: 2px;
+	@apply h-2 bg-offwhite mx-8;
+}
+</style>
--- src/iqc-site/src/views/LandingPage.vue
+++ src/iqc-site/src/views/LandingPage.vue
@@ -1,12 +1,31 @@
 <template>
-	<div></div>
+	<div>
+		<HeroSection>
+			<PageHeader />
+			<div class="mt-auto mb-26 w-2/3 self-center">
+				<p class="tracking-widest mb-2 text-sm font-bold">
+					<span class="text-gold mr-2">00</span> START
+				</p>
+				<h1 class="text-5xl">Quantum on the frontier.</h1>
+				<p class="mt-4">
+					It's been a while since Utah was the "wild west". <br />
+					Now we're back. Welcome to the quantum frontier.
+				</p>
+			</div>
+		</HeroSection>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import PageHeader from '@/components/shared/PageHeader.vue';
+import HeroSection from '@/components/shared/HeroSection.vue';
 
 export default defineComponent({
-	components: {},
+	components: {
+		PageHeader,
+		HeroSection,
+	},
 	mixins: [],
 	props: {},
 	data() {
