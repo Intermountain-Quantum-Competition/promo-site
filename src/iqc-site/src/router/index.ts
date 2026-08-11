10411d11b5498ed79059537ea6884418ae749e23
Caleb Dougal <dougalcaleb@gmail.com>
2026-08-10 21:27:09 -0600

    Replace Sponsors and About page 404s with Coming Soon placeholder

--- src/iqc-site/src/router/index.ts
+++ src/iqc-site/src/router/index.ts
@@ -12,18 +12,18 @@ export const router = createRouter({
 		{
 			path: '/sponsors',
 			name: 'sponsors',
-			component: () => import('@/views/SponsorsPage.vue'),
+			component: () => import('@/views/ComingSoonView.vue'),
 		},
 		{
 			path: '/students',
 			name: 'students',
 			component: () => import('@/views/StudentsPage.vue'),
 		},
-		// {
-		// 	path: '/about',
-		// 	name: 'about',
-		// 	component: () => import('@/views/AboutPage.vue')
-		// },
+		{
+			path: '/about',
+			name: 'about',
+			component: () => import('@/views/ComingSoonView.vue'),
+		},
 		{
 			// Lazy-loaded so it stays out of the initial bundle. Prefer this form for
 			// any route that isn't the landing page.
